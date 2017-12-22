5754daf012e79a37f4141d7063bbbad567461506
mraras <earas446@gmail.com>
2017-12-22 13:35:16 -0800

    Week 1 Upload

---
+++ Week1/10assignment.js
@@ -0,0 +1,15 @@
+"use strict"
+
+
+let multipleTypeArray = ["name", 4];
+console.log(multipleTypeArray)
+
+let x = 6;
+let y = 10;
+if (x / 0 === y / 0) {
+    console.log(true)
+} else {
+    console.log(false)
+}
+
+console.log("Yeah! I`ve done this assignment. This was really very funny!")
---
+++ Week1/1assignment.js
@@ -0,0 +1,6 @@
+"use strict"
+
+
+console.log("Hello,World")
+console.log("Merhaba,Dunya")
+console.log("Hoi,Wereld")
