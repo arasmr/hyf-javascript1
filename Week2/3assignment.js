552319c802f1f2e54ad2c2e8d3cb4add15b56f68
mraras <earas446@gmail.com>
2017-12-22 13:44:03 -0800

    name of file changed

---
+++ Week2/3assignment.js
@@ -0,0 +1,9 @@
+"use strict"
+
+
+var x;
+console.log("the value of my variable x will be:undefined")
+console.log(x)
+x = 40;
+console.log("the value of my variable x will be:40")
+console.log(x)
---
+++ Week2/4assignment.js
@@ -0,0 +1,9 @@
+"use strict"
+
+
+let y = "Hello world";
+console.log("the value of my string will be:Hello world")
+console.log(y)
+y = "How are you?";
+console.log("the value of my string will be:How are you?")
+console.log(y)
